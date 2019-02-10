60355ba99458d482b2665f535390ac0c1dd327de
Bladimir Aponte <bladimirapon@gmail.com>
2019-02-10 13:53:40 -0400

    navegacion entre componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { PromocionesComponent } from './promociones/promociones.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AjustesComponent } from './ajustes/ajustes.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { RecomendadoComponent } from './recomendado/recomendado.component';
+import { FavoritosComponent } from './favoritos/favoritos.component';
+import { ProductoComponent } from './productos/producto/producto.component';
+import { ListaproductosComponent } from './productos/listaproductos/listaproductos.component';
 
 const routes: Routes = [
 
@@ -12,6 +18,12 @@ children:[
 {path: '',redirectTo:'/home',pathMatch: 'full'},
 {path: 'home',component: HomeComponent},
 {path: 'promociones',component: PromocionesComponent},
+{path: 'ajustes',component: AjustesComponent},
+{path: 'carrito',component: CarritoComponent},
+{path: 'recomendado',component: RecomendadoComponent},
+{path: 'favoritos',component: FavoritosComponent},
+{path: 'productos/producto',component: ProductoComponent},
+{path: 'productos/listaproductos',component: ListaproductosComponent},
 
 ],
 
