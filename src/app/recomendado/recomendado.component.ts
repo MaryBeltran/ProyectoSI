8c82e2f05025809be15a5909a4a6e9b6c369669f
Ignacio Rodriguez <supernacho2011@gmail.com>
2019-02-12 00:17:59 -0400

    barra-parte2

--- src/app/recomendado/recomendado.component.ts
+++ src/app/recomendado/recomendado.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecomendadoComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    
+  }
+
 
   ngOnInit() {
+   
   }
+
+ 
 
 }
