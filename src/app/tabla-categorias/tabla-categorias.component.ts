181ed096ce2290f8aa7e14c7d7e4cb2a74ac683c
Bladimir Aponte <bladimirapon@gmail.com>
2019-02-16 15:57:42 -0400

    master actualizado

---
+++ src/app/tabla-categorias/tabla-categorias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-categorias',
+  templateUrl: './tabla-categorias.component.html',
+  styleUrls: ['./tabla-categorias.component.css']
+})
+export class TablaCategoriasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
