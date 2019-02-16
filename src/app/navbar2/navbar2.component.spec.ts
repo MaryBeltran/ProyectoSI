1d4303cb49fcd1b71a863eeab8ffa38b4f9a7b5c
Ignacio Rodriguez <supernacho2011@gmail.com>
2019-02-16 09:56:59 -0400

    actualizacion5

--- src/app/navbar2/navbar2.component.spec.ts
+++ src/app/navbar2/navbar2.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar2.component';
+import { Navbar2Component } from './navbar2.component';
 
 describe('Navbar2Component', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+  let component: Navbar2Component;
+  let fixture: ComponentFixture<Navbar2Component>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ Navbar2Component ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(Navbar2Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
