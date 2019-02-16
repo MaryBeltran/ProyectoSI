3b5397ec9e5157bd8cb7a76dbde5cc2ffbeee99c
Ignacio Rodriguez <supernacho2011@gmail.com>
2019-02-16 13:46:38 -0400

    errorsolucion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { Navbar2Component } from './navbar2/navbar2.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductosComponent } from './productos/productos.component';
 import { FooterComponent } from './footer/footer.component';
