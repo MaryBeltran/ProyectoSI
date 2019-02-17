f69d8b2f7106465133baf84bc695d1a4e1c77716
Ignacio Rodriguez <supernacho2011@gmail.com>
2019-02-16 21:04:57 -0400

    ProductoPrueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,10 +27,10 @@ children:[
 {path: 'carrito',component: CarritoComponent},
 {path: 'recomendado',component: RecomendadoComponent},
 {path: 'favoritos',component: FavoritosComponent},
-  {path: 'productos', children:[ 
+{path: 'productos', children:[ 
       {path: 'producto',component: ProductoComponent},
       {path: 'listaproductos',component: ListaproductosComponent},
-  ]},
+]},
 {path: 'login',component: LoginComponent},
 {path: 'ajustes-usuario', component: AjustesUsuarioComponent},
 {path: 'ajustes-administrador', component: AjustesAdministradorComponent},
