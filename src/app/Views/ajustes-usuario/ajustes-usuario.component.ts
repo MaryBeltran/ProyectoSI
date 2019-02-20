aed36f07d2fe260e0a504754756b19234dc7d6c1
Mary Beltrán <m.beltran@correo.unimet.edu.ve>
2019-02-20 17:33:25 -0400

    conexion fb 2

--- src/app/Views/ajustes-usuario/ajustes-usuario.component.ts
+++ src/app/Views/ajustes-usuario/ajustes-usuario.component.ts
@@ -8,12 +8,25 @@ import { FirestoreService } from 'src/app/Service/firestore.service';
 })
 export class AjustesUsuarioComponent implements OnInit {
   usuarios = [];
+  UsertoEdit;
 
   constructor(private fs: FirestoreService) {
     fs.getAllUsuarios().subscribe(usuarios =>{
       this.usuarios = usuarios
     })
    }
+
+   CambiarAdmin(item){
+    this.UsertoEdit = item
+    console.log(this.UsertoEdit)
+    if(this.UsertoEdit.admin == true){
+      this.UsertoEdit.admin = false
+    }else{
+      this.UsertoEdit.admin = true
+    }
+    console.log(this.UsertoEdit)
+    this.fs.updateUsers(this.UsertoEdit)
+  }
 
   ngOnInit() {
   }
